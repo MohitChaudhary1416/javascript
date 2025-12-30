2aaf30a0c2e2d70c94ae7222dc22c17ad8b5246b
Mohit Chaudhary <mohitchaudhary0514@gmail.com>
2025-12-30 12:17:14 +0000

    Conversion of datatypes

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,33 @@
+let score = "50"
+
+console.log(typeof score);
+
+let valueinnumber = Number(score)
+console.log(typeof valueinnumber)
+
+let marks = "80fm"
+let ValueInNumber = Number(marks)
+console.log(ValueInNumber)
+
+let mark = null
+let value = Number(mark)
+console.log(value)
+
+// "50" => 50
+// "80fm" => NaN
+// true => 1
+// false => 0
+
+let IsLoogedIn = 1
+let booleanisloogedin = Boolean(IsLoogedIn)
+console.log(booleanisloogedin);
+
+// 1 => true
+// 0 => false
+// "" => fasle
+// "mohit" => true
+
+let isNumber = 22
+let stringnumber = String(isNumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
