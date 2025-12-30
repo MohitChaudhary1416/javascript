4c5f28af2c91706d2108c9a81055bcabb420451f
Mohit Chaudhary <mohitchaudhary0514@gmail.com>
2025-12-30 11:50:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //Treat all js code as newer version
+
+//alert(5+5) // we are using node js not browser
+
+// console.log(3
+//     + 3 // code readability should be high
+// )  
+
+console.log("Mohit")
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
