a33cacb44184600c4a6fbc6e9a7b78bf54c41c06
Mohit Chaudhary <mohitchaudhary0514@gmail.com>
2025-12-30 11:50:17 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const account_id = 1501
+let account_email = "mohit@gmail.com"
+let account_password = "12345"
+
+account_city = "Butwal"
+
+// account_id = 555 Not allowed
+
+
+account_email = "raaj@gmail.com"
+account_password = "45678"
+account_city = "jeetpur"
+let account_state;
+
+
+console.log(account_id);
+/*
+prefer not to use var
+because of issue in block scope and functional sc
+*/
+
+console.table([account_email,account_password,account_city,account_state]);
+
+
+
